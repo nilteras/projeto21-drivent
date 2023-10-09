2436ce56f30548bb869fa80708f0bae126da817f
nilteras <niltonrnascimento@hotmail.com>
2023-10-09 12:35:22 -0300

    fix: validation error in /post and /put

--- src/services/booking-service.ts
+++ src/services/booking-service.ts
@@ -1,9 +1,21 @@
 import { notFoundError } from "@/errors";
 import { forbiddenError } from "@/errors/forbidden-error";
+import { enrollmentRepository, ticketsRepository } from "@/repositories";
 import { bookingsRepository } from "@/repositories/booking-repository";
+import { TicketStatus } from "@prisma/client";
 
 
-async function createBooking(userId: number, roomId: number){
+async function createBooking(userId: number, roomId: number) {
+    const enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
+
+    const ticket = await ticketsRepository.findTicketByEnrollmentId(enrollment.id);
+    const type = ticket.TicketType;
+
+    if (ticket.status === TicketStatus.RESERVED || type.isRemote || !type.includesHotel) {
+        throw forbiddenError();
+    }
+
+    await capacityRoom(roomId);
 
     const result = await bookingsRepository.createBookingDB(userId, roomId);
 
@@ -15,9 +27,9 @@ async function createBooking(userId: number, roomId: number){
     return booking;
 }
 
-async function getBookings(userId: number){
+async function getBookings(userId: number) {
     const result = await bookingsRepository.getBookingsDB(userId);
-    if(!result) throw notFoundError();
+    if (!result) throw notFoundError();
 
     const booking = {
         id: result.id,
@@ -27,18 +39,26 @@ async function getBookings(userId: number){
     return booking;
 }
 
-async function updateBooking(userId: number, bookingId: number, roomId: number){
+async function updateBooking(userId: number, bookingId: number, roomId: number) {
     const userBooking = await bookingsRepository.getBookingsDB(userId);
-     if(!userBooking || userBooking.id !== bookingId) throw forbiddenError(); 
+    if (!userBooking || userBooking.id !== bookingId) throw forbiddenError();
+    
+    await capacityRoom(roomId);
 
-     const result = await bookingsRepository.updateBookingDB(bookingId, roomId);
+    const result = await bookingsRepository.updateBookingDB(bookingId, roomId);
 
-     const booking = {
+    const booking = {
         bookingId: result.id,
-        Room: result.Room 
-     };
+        Room: result.Room
+    };
 
-     return booking;
+    return booking;
+}
+
+async function capacityRoom(roomId: number) {
+    const room = await bookingsRepository.getRoomIdDB(roomId);
+    if (!room) throw notFoundError();
+    if (room.capacity <= room.Booking.length) throw forbiddenError();
 }
 
 export const bookingsService = {
